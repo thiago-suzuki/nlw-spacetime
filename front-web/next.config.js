44c1314edf4442b5b9fa8a142cf3292fd5f73ce1
Thiago Ikenaga Suzuki <dev7.gbmlogistica@outlook.com>
2023-05-21 21:24:02 -0300

    Initial Commit

---
+++ front-web/next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['avatars.githubusercontent.com', 'revistakoreain.com.br'],
+  },
+}
+
+module.exports = nextConfig
